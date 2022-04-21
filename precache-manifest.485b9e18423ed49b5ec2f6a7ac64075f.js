9bcab064661df15d4e6a6b5f39e91188a2800ece
Code Sandbox <hello@codesandbox.io>
2022-04-21 19:11:21 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.485b9e18423ed49b5ec2f6a7ac64075f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409b52c7dc2b0a5a6dfc6490013a5897",
+    "url": "https://.github.io/csb-ljsgge/index.html"
+  },
+  {
+    "revision": "90dde675357cc82e09f4",
+    "url": "https://.github.io/csb-ljsgge/static/css/main.a79fa62c.chunk.css"
+  },
+  {
+    "revision": "f9e0bd6d69c827debefb",
+    "url": "https://.github.io/csb-ljsgge/static/js/2.cf014812.chunk.js"
+  },
+  {
+    "revision": "90dde675357cc82e09f4",
+    "url": "https://.github.io/csb-ljsgge/static/js/main.e6913604.chunk.js"
+  },
+  {
+    "revision": "3ae1dfc037cb65a7f43d",
+    "url": "https://.github.io/csb-ljsgge/static/js/runtime-main.82cb04a3.js"
+  },
+  {
+    "revision": "1926d64576fdcaac5d6d677df10e1366",
+    "url": "https://.github.io/csb-ljsgge/static/media/233.1926d645.jpg"
+  },
+  {
+    "revision": "ab7b6e3aa300bd05bd5bb0ab30aa4647",
+    "url": "https://.github.io/csb-ljsgge/static/media/diamond.ab7b6e3a.glb"
+  }
+]);
