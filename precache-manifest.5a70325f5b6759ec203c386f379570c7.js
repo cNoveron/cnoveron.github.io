d8ef7a9e32635ce3853055f7cd509b3258c66d18
Carlos Noverón <carlos.escom.ipn@gmail.com>
2022-04-21 14:52:17 -0500

    Update precache-manifest.5a70325f5b6759ec203c386f379570c7.js

--- precache-manifest.5a70325f5b6759ec203c386f379570c7.js
+++ precache-manifest.5a70325f5b6759ec203c386f379570c7.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "d39c8cae16e0d3f2d08a8c8bbc571d4f",
-    "url": "https://.github.io/csb-ljsgge/index.html"
+    "url": "https://cnoveron.github.io/csb-ljsgge/index.html"
   },
   {
     "revision": "d046635a00aa0c81cbbb",
-    "url": "https://.github.io/csb-ljsgge/static/css/main.a79fa62c.chunk.css"
+    "url": "https://cnoveron.github.io/csb-ljsgge/static/css/main.a79fa62c.chunk.css"
   },
   {
     "revision": "8683465991d82cfbe37c",
-    "url": "https://.github.io/csb-ljsgge/static/js/2.3de611a7.chunk.js"
+    "url": "https://cnoveron.github.io/csb-ljsgge/static/js/2.3de611a7.chunk.js"
   },
   {
     "revision": "d046635a00aa0c81cbbb",
-    "url": "https://.github.io/csb-ljsgge/static/js/main.0424b4ee.chunk.js"
+    "url": "https://cnoveron.github.io/csb-ljsgge/static/js/main.0424b4ee.chunk.js"
   },
   {
     "revision": "3ae1dfc037cb65a7f43d",
-    "url": "https://.github.io/csb-ljsgge/static/js/runtime-main.82cb04a3.js"
+    "url": "https://cnoveron.github.io/csb-ljsgge/static/js/runtime-main.82cb04a3.js"
   },
   {
     "revision": "1600b9e7bd3baa116c3c723479592628",
-    "url": "https://.github.io/csb-ljsgge/static/media/233.1600b9e7.jpg"
+    "url": "https://cnoveron.github.io/csb-ljsgge/static/media/233.1600b9e7.jpg"
   },
   {
     "revision": "ab7b6e3aa300bd05bd5bb0ab30aa4647",
-    "url": "https://.github.io/csb-ljsgge/static/media/diamond.ab7b6e3a.glb"
+    "url": "https://cnoveron.github.io/csb-ljsgge/static/media/diamond.ab7b6e3a.glb"
   }
 ]);
